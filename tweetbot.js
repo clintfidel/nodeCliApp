0cdb540fda3be4b04e1fe774cff2088c72e90b9b
clintfidel <clintfidel@icloud.comgit config --global user.email clintfidel@icloud.comgit config --global user.email clintfidel@icloud.com>
2017-04-11 15:13:53 +0100

    Added function to communicate with twitter API

---
+++ tweetbot.js
@@ -0,0 +1,17 @@
+var tweetBotJs = require('twit');
+
+var Twitter = new  tweetBotJs({
+    consumer_key:         'yVR7czqz0wkrixWP4rLzYYvRm' //  Consumer Key
+  , consumer_secret:      'vbZlrJ3d6MsOIPG7bIVHo4OJnK8VJE1v1jAHfsDWxy21gpYrGb' //  Consumer Secret
+  , access_token:         '1382063460-nFQvtrzoJRtmtVXd4PuJWZNkHzmHW7GsepFiLM1' // Access Token
+  , access_token_secret:  'qjEX6YfWs8CshKmrcFKGlyUxMouDYTLweoNmMfMLsIrDx' //  Access Token Secret
+});
+
+Twitter.post('statuses/update', { status: "New tweet from my node app tweetBot" }, function(error, data, response) {
+  if(error) {
+    console.log("There was a problem tweeting the message.", error);
+  }
+  else{
+  	console.log("Status Updated Successfully!");
+  }
+});
